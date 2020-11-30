28906b0c9a508db124ad064ef4c6877267886f69
enaburekhan <ericenaburekhan2018@gmail.com>
2020-11-30 05:46:11 +0100

    create post and modified route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
-    PASSWORD_REQUIRMENTS = /\A
-    (?=.*\d)           # Must contain a digit
-    (?=.*[a-z])        # Must contain a lower case character
-    (?=.*[A-Z])        # Must contain an upper case characte
-    /x.freeze
-    validates :name, presence: true, uniqueness: true, length: { minimum: 4, maximum: 20 }
-    validates :email, presence: true, uniqueness: true, length: { minimum: 5, maximum: 20 }
-    validates :password, presence: true, length: { minimum: 6, maximum: 20 }, format: { with: PASSWORD_REQUIRMENTS }    
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :posts       
+#     PASSWORD_REQUIRMENTS = /\A
+#     (?=.*\d)           # Must contain a digit
+#     (?=.*[a-z])        # Must contain a lower case character
+#     (?=.*[A-Z])        # Must contain an upper case characte
+#     /x.freeze
+#     validates :name, presence: true, uniqueness: true, length: { minimum: 4, maximum: 20 }
+#     validates :email, presence: true, uniqueness: true, length: { minimum: 5, maximum: 20 }
+#     validates :password, presence: true, length: { minimum: 6, maximum: 20 }, format: { with: PASSWORD_REQUIRMENTS }    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_130432) do
+ActiveRecord::Schema.define(version: 2020_11_29_195348) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "Author_name"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
